38d136ba081c42ccf88ef587aac9c600b6361e4d
Ben <benlaugherty@gmail.com>
2019-12-11 17:12:59 -0800

    [add] a ton of stuff :)

--- app.py
+++ app.py
@@ -12,6 +12,14 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
